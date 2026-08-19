a232461d1118641a1cd18d999cfed2b5c25f7575
Chhun Phynann <Phynann10@gmail.com>
2026-08-19 15:35:21 +0700

    Task 3.1: Created TweetController and configured User-Tweet Eloquent relationships

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Tweet extends Model
 {
-    //
+    use HasFactory;
+
+    // Allow the body and user_id to be saved to the database
+    protected $fillable = [
+        'body',
+        'user_id'
+    ];
+
+    // A Tweet belongs to a specific User
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
