ad43af10051bf1e23c2481eef68bc91081cb0b94
Pom Chantrea <chantreapom585@gmail.com>
2026-08-19 14:43:26 +0700

    Task 2.2: Designed responsive public profile UI using Tailwind

--- app/Http/Controllers/PublicProfileController.php
+++ app/Http/Controllers/PublicProfileController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PublicProfileController extends Controller
 {
-    //
+    public function show(User $user)
+    {
+        // We will pass the user to a view (which Teammate A will build in Task 2.2)
+        return view('profile.public', [
+            'user' => $user
+        ]);
+    }
 }
